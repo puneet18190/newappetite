f67f1d57cd81f5934ec4e9f9e73cd8bddea60bd3
Brandon Schatz <brandon@sportsphotos.com>
2014-05-14 08:43:16 +0000

    fixing some player images and making the landing page a one page system

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+	def devise_mapping
+	  Devise.mappings[:user]
+	end
+
+	def resource_name
+	  devise_mapping.name
+	end
+
+	def resource_class
+	  devise_mapping.to
+	end
+	def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
