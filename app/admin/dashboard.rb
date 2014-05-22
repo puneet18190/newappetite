2873019fe4202a3d22a30943bbcf89d7da92c50d
Brandon Schatz <brandon@sportsphotos.com>
2014-05-22 01:33:40 +0000

    fixed some merge conflicts

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-ActiveAdmin.register_page "Dashboard" do
-
-  menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
-
-  content title: proc{ I18n.t("active_admin.dashboard") } do
-    columns do
-       column do
-         panel "Invited Users" do
-           ul do
-             User.where(:invite_user => false).map do |post|
-               li link_to(post.email) do
-                 button class: "invite_user",id: "#{post.id}" do
-                   "Invite"
-                end
-               end
-             end
-           end
-         end
-       end
-    end
-  end
-end
-=======
 ActiveAdmin.register_page "Dashboard" do
 
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
@@ -54,4 +30,3 @@ ActiveAdmin.register_page "Dashboard" do
     end
   end
 end
->>>>>>> origin/active_admin
